ec687ad95af9644283f93f33cfda8349fe46b3f9
Pitchumani Sivanupandi <s.pitchumani@gmail.com>
2020-11-23 22:22:31 +0530

    Update paths respective to pwa directory

--- pwa/js/main.js
+++ pwa/js/main.js
@@ -1,15 +1,18 @@
-function registerSW() {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
+window.onload = () => {
+    'user strict';
+    function registerSW() {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/pwa/sw.js').then(function(registration) {
+                    // Registration was successful
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(err) {
+                    // registration failed :(
+                    console.log('ServiceWorker registration failed: ', err);
+                });
             });
-        });
+        }
     }
-}
 
-registerSW()
+    registerSW()
+}
